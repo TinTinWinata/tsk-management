4c1ce8da4cb817eafc07c4004bf59ad3abb6751e
TinTinWinata <tintin6892@gmail.com>
2024-12-05 00:12:45 +0700

    feat: initiate google

---
+++ database/migrations/2024_12_04_170223_create_google_schedules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('google_schedules', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(Str::uuid());
+            $table->dateTime('date');
+            $table->string('title');
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('google_schedules');
+    }
+};
